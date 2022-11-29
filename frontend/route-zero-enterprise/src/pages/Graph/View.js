bddf11dab442ef91320c8a770d772afa0eadf949
Smaranda Tasmoc <fo21366@bristol.ac.uk>
2022-11-29 00:21:44 +0000

    trying GET request in axios for graphs

--- frontend/route-zero-enterprise/src/pages/Graph/View.js
+++ frontend/route-zero-enterprise/src/pages/Graph/View.js
@@ -1,25 +1,57 @@
-import React, {useEffect} from "react";
+import React, {Component, useEffect, ReactDOM} from "react";
 import {Container} from "react-bootstrap";
 
 import {readText} from '../../helpers/file_reader.js';
 import {Chart} from '../../components/Chart/Chart.js';
 
+//import axios from '../../../node_modules/axios';
+
 import "./View.scss";
+
+const routeZeroApi = "https://emissions-qzzieui6kq-ew.a.run.app"
+
+const api_key = "replace-me-with-api-key"
+
+axios.get(routeZeroApi, {}, {
+    auth: {
+      api_key
+    }
+  })
+  .then(function(response) {
+    console.log(response.data, 'api response');
+  })
 
 export const View = (props) => {
 
     useEffect(() => {
         document.title = "Graphs | RouteZero"
     }, []);
-    
+
     return(<>
             <h1>Visualisation</h1>
             <p>{JSON.stringify(props.response)}</p>
-            <div>
-                <Chart/>
-                <Chart/>
-                <Chart/>
-                <Chart/>
+
+            getCustomerData()
+
+            <div className="center-grid">
+                <div className="cell">
+                    <h2>Before</h2>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                </div>
+                <div className="cell">
+                    <h2>After</h2>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                </div>
             </div>
 
         </>
